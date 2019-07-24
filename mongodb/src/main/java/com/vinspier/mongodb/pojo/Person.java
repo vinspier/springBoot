120ff3f9da36380b3fcb6898d69b3dc9e9b77a40
vinspier <954579147@qq.com>
2019-07-24 18:17:52 +0800

    add mongodb project demo

--- mongodb/src/main/java/com/vinspier/mongodb/pojo/Person.java
+++ mongodb/src/main/java/com/vinspier/mongodb/pojo/Person.java
@@ -1,9 +1,19 @@
 package com.vinspier.mongodb.pojo;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@TypeAlias("pers")
+@Document
 public class Person {
+    @Id
     private String id;
     private String name;
     private int age;
+    @Version
+    private Long version;
 
     public Person(String name, int age) {
         this.name = name;
@@ -18,6 +28,27 @@ public class Person {
     }
     public int getAge() {
         return age;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 
     @Override
--- mongodb/src/test/java/com/vinspier/mongodb/MongodbApplicationTests.java
+++ mongodb/src/test/java/com/vinspier/mongodb/MongodbApplicationTests.java
@@ -9,10 +9,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,14 +31,74 @@ public class MongodbApplicationTests {
 
 	}
 
+	MongoOperations mongoOps = new MongoTemplate(new MongoClient(), "vinspier");
+	Person p = new Person("sdd", 23);
+
 	@Test
 	public void save(){
-		MongoOperations mongoOps = new MongoTemplate(new MongoClient(), "vinspier");
-		mongoOps.insert(new Person("Joe", 34));
+		// Insert is used to initially store the object into the database.
+		mongoOps.insert(p);
+		log.info("=========================================== person id: " + p.getId());
+		log.info("=========================================== insert into " + p);
+	}
 
-		log.info(mongoOps.findOne(new Query(where("name").is("Joe")), Person.class));
+	@Test
+	public void find(){
+		// Find
+		p = mongoOps.findById(p.getId(), Person.class);
+		log.info("=========================================== Found: " + p);
+	}
 
-		// mongoOps.dropCollection("person");
+	@Test
+	public void updateP(){
+		// Update
+		mongoOps.updateFirst(query(where("name").is("fxb")), update("age", 35), Person.class);
+		p = mongoOps.findOne(query(where("name").is("fxb")), Person.class);
+		log.info("=========================================== Updated: " + p);
+	}
+
+	@Test
+	public void delete(){
+		// Delete
+		p.setId("5d367f4478d7571eb802e643");
+		p = mongoOps.findById(p.getId(), Person.class);
+		mongoOps.remove(p);
+	}
+
+	@Test
+	public void findAll(){
+		// Check that deletion worked
+		List<Person> peopleList =  mongoOps.findAll(Person.class);
+		StringBuilder sb = new StringBuilder();
+		for (Person p : peopleList){
+			sb.append(p).append("\n");
+		}
+		log.info("=====================================Number of people = : " + sb);
+	}
+
+	@Test
+	public void drop(){
+		mongoOps.dropCollection(Person.class);
+	}
+
+	@Test
+	public void findAndModify(){
+		Query query = new Query(Criteria.where("name").is("fxb"));
+		Update update = new Update().inc("age", 100);
+		Person p = mongoOps.findAndModify(query, update, Person.class); // return's old person object
+		log.info("===================================== " + p);
+	}
+
+	@Test
+	public void testOptimisticLock(){
+		Person person = new Person("optLock",100);
+		mongoOps.insert(person);
+		log.info("======================== id" + person.getId());
+		Person tmp = mongoOps.findOne(query(where("id").is(person.getId())),Person.class);
+		person.setAge(200);
+		tmp.setAge(150);
+		mongoOps.save(person);
+		mongoOps.save(tmp);
 	}
 
 }
